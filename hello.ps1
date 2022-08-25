0d487b72b5db496648a8b3797e6a78919d63c103
sdyakub777 <105699189+sdyakub777@users.noreply.github.com>
2022-08-25 16:19:52 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,2 +1,4 @@
-#!/usr/bin/pwsh -Command
-echo "Hello World!"
+$Password = $env:Password | ConvertTo-SecureString -AsPlainText -Force
+$creddentials = New-Object System.Management.Automation.PSCredential -ArgumentList $env:UserName, $Password
+Connect-PowerBIServiceAccount -Credential $creddentials
+New-PowerBIReport -Path $env:location -WorkspaceId 78a193d7-9d67-4291-a5c6-47e4482d4d41
